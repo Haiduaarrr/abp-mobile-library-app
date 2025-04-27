3f3a03462ec005bc2e5803910f8ca3db64cbcaac
Ibnuirza <ganesha.irza@gmail.com>
2025-04-27 22:20:12 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:library_app_abp/library_app.dart';
-import 'package:http/http.dart' as http;
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 
 void main() async {
-  // Ini diperlukan untuk memastikan Flutter diinisialisasi sebelum Firebase
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Inisialisasi Firebase
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   // Jalankan aplikasi Anda
   runApp(const LibraryApp());
